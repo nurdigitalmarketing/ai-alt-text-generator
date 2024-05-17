16a1a2d699d64626dd326719d977d759f028b60f
NUR® Digital Marketing <163868030+nurdigitalmarketing@users.noreply.github.com>
2024-05-17 14:18:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with col1:
 
 # Colonna per il titolo e il testo "by NUR® Digital Marketing" (al centro)
 with col2:
-    st.title('Integrazione AltText.ai con Streamlit')
+    st.title('Alt Text AI App')
     st.markdown('###### by [NUR® Digital Marketing](https://www.nur.it)')
 
 st.markdown("""
